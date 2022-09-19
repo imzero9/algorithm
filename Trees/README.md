38ce3cab29a99b7c2416972243b22519ed749d4a
Nayoung-Gu <skdud000322@naver.com>
2022-09-19 22:28:00 +0900

    Introduction of trees

---
+++ Trees/README.md
@@ -0,0 +1,21 @@
+# Trees
+
+## PROS
+
+-   Better than O(n)
+-   Ordered
+-   Flexible Size
+
+## CONS
+
+-   No O(1) operations
+
+## Binary Tree Node Using Class
+
+```js
+function BinaryTreeNode(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+```
---
+++ Trees/exercise1.js
@@ -0,0 +1,28 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node();
+    }
+
+    lookup(value) {}
+}
+
+const tree = new BinarySearchTree();
+
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
