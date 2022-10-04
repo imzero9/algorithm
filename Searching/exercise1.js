aa8558ae8144b564d44cef619ecf9d82e7273851
Nayoung Gu <skdud000322@naver.com>
2022-10-04 18:52:57 +0900

    BFS and DFS exercise

---
+++ Searching/exercise1.js
@@ -0,0 +1,11 @@
+// If you know a solution is not far from the root of the tree: BFS
+
+// If the tree is very deep and solutions are rare: BFS (tree is very deep and DFS takes long time)
+
+// If the tree is very wide: DFS (BFS needs too much memory)
+
+// If solutions are frequent but located deep in the tree: DFS
+
+// Determining whether a path exists between two nodes: DFS
+
+// Finding the shortest path: BFS
