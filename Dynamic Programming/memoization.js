b7136afb7c70889c2faceaf3865d88165f3ddade
Nayoung-Gu <skdud000322@naver.com>
2022-10-05 14:51:49 +0900

    Memoization practice

---
+++ Dynamic Programming/memoization.js
@@ -0,0 +1,19 @@
+function addTo80(n) {
+    console.log("long time");
+    return n + 80;
+}
+
+function memoizedAddTo80() {
+    let cache = {};
+    return function (n) {
+        if (n in cache) {
+            return cache[n];
+        } else {
+            console.log("long time");
+            cache[n] = n + 80;
+            return cache[n];
+        }
+    };
+}
+
+const memoized = memoizedAddTo80();
