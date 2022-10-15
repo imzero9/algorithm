f81655c7ca3722fe61e07a78fb9b291a0438ee5e
Nayoung Gu <skdud000322@naver.com>
2022-10-15 21:24:10 +0900

    reverseString function using reverse()

---
+++ LeetCode/reverseString.js
@@ -0,0 +1,7 @@
+// Write a function that reverses a string. The input string is given as an array of characters s and the output is an array.
+
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+const reverseString = (s) => {
+    return s.reverse();
+};
